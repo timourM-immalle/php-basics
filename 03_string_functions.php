08ac1aded055c2051c42d9c4d1d460b4fd891217
timourM-immalle <timour.meeusen@immalle.eu>
2018-01-11 08:54:36 +0100

    detail ...

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -7,8 +7,8 @@ echo str_word_count($zin);
 
 // Vervang in $zin "lente" door "zomer" en druk af.
 $woorden = str_word_count($zin, 1);
-$seizoen = array_search("Lente", $woorden); //heeft een 0 based index
-$vervanging = array($seizoen => "Zomer");
+$seizoen = array_search("lente", $woorden); //heeft een 0 based index
+$vervanging = array($seizoen => "zomer");
 $nieuweWoorden = array_replace($woorden, $vervanging);
 
 echo '<br>' . implode(" ", $nieuweWoorden) . "." . '<br>';
