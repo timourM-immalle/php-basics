822091b6246feecbfac9fbca216e6c751b372868
timourM-immalle <timour.meeusen@immalle.eu>
2018-01-11 09:36:25 +0100

    3e werkt nog niet ...

--- 06_arrays.php
+++ 06_arrays.php
@@ -1,17 +1,25 @@
 <?php
 
 // Maak een Array met 3 namen van medeleerlingen.
+$collegae = array("Mathias", "Timour", "Karam");
 
+// Itereer er over met een for-loop. Print ze af in een HTML-ongenummerde lijst.
+echo '<ul>';
+for ($i = 0; $i < count($collegae); $i++)
+{
+	echo '<li>' . $collegae[$i];
+}
 
-// Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
-
-
-// Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
-
+// Itereer er over met een foreach-loop. Print ze af in HTML-genummerde lijst.
+echo '<ol>';
+foreach ($collega as $collegae)
+{
+	echo '<li>' . $collega;
+}
 
 
 $naam = "Willy";
 
-// Itereer met een for-loop over de variable $naam alsof het een array was.
+// Itereer met een for-loop over de variable $naam alsof het een array was. (Dus de letters 1 voor 1 afdrukken.)
 
 ?>
