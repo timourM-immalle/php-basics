22df9594ce7c7773a2af28943bb730fa25e42cb4
timourM-immalle <timour.meeusen@immalle.eu>
2018-01-11 08:52:20 +0100

    oef. klaar

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -3,9 +3,14 @@
 $zin = "Binnenkort is het weeral lente";
 
 // Print af hoeveel woorden $zin bevat.
-
+echo str_word_count($zin);
 
 // Vervang in $zin "lente" door "zomer" en druk af.
+$woorden = str_word_count($zin, 1);
+$seizoen = array_search("Lente", $woorden); //heeft een 0 based index
+$vervanging = array($seizoen => "Zomer");
+$nieuweWoorden = array_replace($woorden, $vervanging);
 
+echo '<br>' . implode(" ", $nieuweWoorden) . "." . '<br>';
 
 ?>
