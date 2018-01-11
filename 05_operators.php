b69fe21a25d34919d10b22f0d058dec331ca6497
timourM-immalle <timour.meeusen@immalle.eu>
2018-01-11 09:13:39 +0100

    oef. gemaakt

--- 05_operators.php
+++ 05_operators.php
@@ -1,10 +1,10 @@
 <?php
 
 // Bereken de rest bij gehele deling van 10 door 3.
-
+echo 10 % 3;
 
 // Bereken de 5e macht van 2.
-
+echo pow(2, 5);
 
 
 $var_a = "123";
@@ -12,11 +12,11 @@ $var_b = 123;
 
 // Vervang de true in de if-voorwaarden zodat het klopt wat er staat:
 
-if(true) {
+if($var_a == $var_b && $var_a !== $var_b) { //"and" i.p.v. "&&" is ook goed (denk ik)
     echo '$var_a en $var_b zijn aan elkaar gelijk maar zijn niet van hetzelfde type.';
 }
 
-if(true) {
+if($var_a === $var_b) {
     echo '$var_a en $var_b zijn aan elkaar gelijk en van hetzelfde type.';
 }
 
