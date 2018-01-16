48b15d00bc11dd9c002bf32b4c6075ab8e76851b
timourM-immalle <timour.meeusen@immalle.eu>
2018-01-16 14:31:13 +0100

    nog niet kunnen testen maar ik denk: oef. klaar

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -15,5 +15,28 @@ $punten = 0;
 // I 1 punt
 // N 1 punt
 
+for ($i = 0; strlen($woord); $i++)
+{
+	$letter = $woord[$i];
+	
+	switch ($letter)
+	{
+		case "q":
+		   $punten += 10;
+		   break;
+		case "u":
+		   $punten += 4;
+		   break;
+		case "g":
+		   $punten += 3;
+		   break;
+		case "t" || "r":
+		   $punten += 2;
+		   break;
+		case "o" || "e" || "i" || "n":
+		   $punten += 1;
+		   break;
+	}
+}
 
 ?>
